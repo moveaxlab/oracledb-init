e5a276cb35a644d33fb4e1733da1dcb90343769c
lcesta <luca.cesta@moveax.it>
2024-04-18 12:44:50 +0200

    change base image

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
-from container-registry.oracle.com/database/free:23.3.0.0
+FROM fedora:39
 COPY --chmod=777 docker-entrypoint.sh /usr/local/bin/
 ENTRYPOINT ["docker-entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,14 @@
 
 set -euo pipefail
 
+dnf update -y
+
+curl https://download.oracle.com/otn_software/linux/instantclient/1922000/oracle-instantclient19.22-basic-19.22.0.0.0-1.x86_64.rpm -o instant.rpm
+curl https://download.oracle.com/otn_software/linux/instantclient/1922000/oracle-instantclient19.22-sqlplus-19.22.0.0.0-1.x86_64.rpm -o sqlplus.rpm
+
+yum --nogpgcheck localinstall instant.rpm -y
+yum --nogpgcheck localinstall sqlplus.rpm -y
+
 sqlplus $ADMIN_USER/$ADMIN_PASSWORD@$dsn <<EOF
 declare
 userexist integer;
